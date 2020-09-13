c08ad3a9b304bf2160565855d6bbf3eb9ff84da5
Arafath1019 <arafath.yeasin1019@gmail.com>
2020-09-14 00:52:58 +0600

    Updating reverse number code

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,29 +1,25 @@
 class Solution:
     def reverse(self, x: int):
         reverse = 0
-        if (-2)**31 <= x and x <= (2**31)-1:
-            if x > 0:
-                while x != 0:
-                    remainder = x % 10
-                    if reverse > (2147483647/10):
-                        return 0
-                    reverse = reverse * 10 + remainder
-                    x = int(x / 10)
-                return reverse
+        if x > 0:
+            while x != 0:
+                remainder = x % 10
+                if reverse > (2147483647/10):
+                    return 0
+                reverse = reverse * 10 + remainder
+                x = int(x / 10)
+            return reverse
 
-            elif x < 0:
-                x = x * (-1)
-                while x != 0:
-                    remainder = x % 10
-                    if reverse > ((2147483648)/10):
-                        return 0
-                    reverse = reverse * 10 + remainder
-                    x = int(x / 10)
-                reverse = reverse * (-1)
-                return reverse
+        elif x < 0:
+            x = x * (-1)
+            while x != 0:
+                remainder = x % 10
+                if reverse > ((2147483648)/10):
+                    return 0
+                reverse = reverse * 10 + remainder
+                x = int(x / 10)
+            reverse = reverse * (-1)
+            return reverse
 
-            else: 
-                return 0
-        
         else: 
             return 0
